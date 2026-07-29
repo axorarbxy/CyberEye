9be52936afc42a93e7676b8ed93fe37ac6d2a5de
chopadediksha2007 <chopadediksha2007@gmail.com>
2026-07-29 15:39:04 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import UrlScanner from "./pages/UrlScanner";
+import Dashboard from "./pages/Dashboard";
 
 function Home() {
   return (
@@ -15,12 +16,14 @@ function App() {
     <BrowserRouter>
       <nav style={{ padding: "16px", borderBottom: "1px solid #ccc" }}>
         <Link to="/" style={{ marginRight: "16px" }}>Home</Link>
-        <Link to="/url-scanner">URL Scanner</Link>
+        <Link to="/url-scanner" style={{ marginRight: "16px" }}>URL Scanner</Link>
+        <Link to="/dashboard">Dashboard</Link>
       </nav>
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/url-scanner" element={<UrlScanner />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );

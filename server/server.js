bf01db338c549c5d7c713481ecdd119ac71c8736
bhaktikadam2808-glitch <bhaktikadam2808@gmail.com>
2026-07-29 15:43:22 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
+const dns = require('dns');
+dns.setServers(['8.8.8.8', '1.1.1.1']);
+
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
 
 const scanRoutes = require('./routes/scanRoutes');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -13,18 +17,19 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.json({status: 'Cybereye API running' });
+  res.json({ status: 'Cybereye API running' });
 });
 
 app.use('/api/scan', scanRoutes);
+app.use('/api/auth', authRoutes);
 
 mongoose.connect(MONGO_URI)
   .then(() => {
     console.log('MongoDB connected');
     app.listen(PORT, () => {
-      console.log('Server running on port ${PORT} ');
+      console.log(`Server running on port ${PORT}`);
     });
   })
   .catch((err) => {
-    console.error('MongoDB connection failed', err.message);
+    console.error('MongoDB connection failed:', err.message);
   });

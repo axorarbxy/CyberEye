4245036babd01a5d2e6e69944e73a12c54364f28
bhaktikadam2808-glitch <bhaktikadam2808@gmail.com>
2026-07-29 15:45:10 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import UrlScanner from "./pages/UrlScanner";
 import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
 
 function Home() {
   return (
@@ -17,13 +18,15 @@ function App() {
       <nav style={{ padding: "16px", borderBottom: "1px solid #ccc" }}>
         <Link to="/" style={{ marginRight: "16px" }}>Home</Link>
         <Link to="/url-scanner" style={{ marginRight: "16px" }}>URL Scanner</Link>
-        <Link to="/dashboard">Dashboard</Link>
+        <Link to="/dashboard" style={{ marginRight: "16px" }}>Dashboard</Link>
+        <Link to="/login">Login</Link>
       </nav>
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/url-scanner" element={<UrlScanner />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );

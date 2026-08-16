3e7d936bdf376481870812a45df870e6f8c6bfc3
shivaknyachiwde <shivaknyachiwade@gmail.com>
2026-08-17 00:16:17 +0530

    Update train_model.py

--- ml-service/app/train_model.py
+++ ml-service/app/train_model.py
@@ -23,7 +23,7 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-model = RandomForestClassifier(n_estimators=100, random_state=42)
+model = RandomForestClassifier(n_estimators=100, random_state=42, class_weight='balanced')
 model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)

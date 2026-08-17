b8facbc43f97178dd917952fd714953706d2a3b5
shivaknyachiwde <shivaknyachiwade@gmail.com>
2026-08-17 09:59:46 +0530

    Update main.py

--- ml-service/app/main.py
+++ ml-service/app/main.py
@@ -1,22 +1,25 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+import joblib
 from extract_features import extract_features
 
 app = FastAPI()
+model = joblib.load("phishing_model.pkl")
 
 class URLRequest(BaseModel):
-  url: str
+    url: str
 
 @app.get("/")
 def health_check():
-  return {"status":"Cybereye ML service running"}
+    return {"status": "Cybereye ML service running"}
 
 @app.post("/predict-url")
-def predict_url(request: URL Request):
-  features = extract_features(request.url)
-
-  return{
-    "url": request.url,
-    "features": features,
-    "status": "model not trained yet"
-  }
+def predict_url(request: URLRequest):
+    features = extract_features(request.url)
+    prediction = model.predict([features])[0]
+    probability = model.predict_proba([features])[0].max()
+    return {
+        "url": request.url,
+        "is_phishing": bool(prediction),
+        "confidence": round(float(probability), 3)
+    }

3330193a0bfeb284497451174e724138cd873b23
bhaktikadam2808-glitch <bhaktikadam2808@gmail.com>
2026-08-19 07:31:59 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ dns.setServers(['8.8.8.8', '1.1.1.1']);
 
 const express = require('express');
 const mongoose = require('mongoose');
+const chatbotRoutes = require('./routes/chatbotRoutes');
 const cors = require('cors');
 require('dotenv').config();
 
@@ -15,6 +16,7 @@ const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/cybereye';
 
 app.use(cors());
 app.use(express.json());
+app.use('/api/chatbot', chatbotRoutes);
 
 app.get('/', (req, res) => {
   res.json({ status: 'Cybereye API running' });
